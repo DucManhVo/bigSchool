113c7563f7fbe08525e66f018a6c952a69d89b12
Duc Manh <voducmanh000@gmail.com>
2022-06-10 16:06:34 +0700

    Page 52 -Done

--- bigSchool/Models/AccountViewModels.cs
+++ bigSchool/Models/AccountViewModels.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace bigSchool.Models
 {
+
     public class ExternalLoginConfirmationViewModel
     {
         [Required]
@@ -79,6 +80,10 @@ namespace bigSchool.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
     }
 
     public class ResetPasswordViewModel
