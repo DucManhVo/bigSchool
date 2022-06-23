888a65c4e93c686f06b9453058714a69afd0cddb
Duc Manh <voducmanh000@gmail.com>
2022-06-23 22:22:13 +0700

    Page 62 ...

---
+++ bigSchool/Controllers/FollowingsController.cs
@@ -0,0 +1,44 @@
+﻿using bigSchool.DTOs;
+using bigSchool.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using static System.Net.Mime.MediaTypeNames;
+using HttpPostAttribute = System.Web.Mvc.HttpPostAttribute;
+
+namespace bigSchool.Controllers
+{
+    public class FollowingsController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        
+        public FollowingsController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Follow(FollowingDto followingDto)
+        {
+            var userId = User.Identity.GetUserId();
+            if (_dbContext.Followings.Any(f => f.FollowerId == userId && f.FolloweeId == followingDto.FolloweeId))
+            {
+                return BadRequest
+            }
+                
+            var following = new Following
+            {
+                FollowerId = userId,
+                FolloweeId = followingDto.FolloweeId
+            };
+            _dbContext.Followings.Add(following);
+            _dbContext.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
