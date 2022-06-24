728f961242fc53b78da0c86dc270f4e60a81c992
Duc Manh <voducmanh000@gmail.com>
2022-06-24 12:41:42 +0700

    Page 63 - Done

--- bigSchool/Controllers/FollowingsController.cs
+++ bigSchool/Controllers/FollowingsController.cs
@@ -1,18 +1,16 @@
-﻿using bigSchool.DTOs;
-using bigSchool.Models;
-using Microsoft.AspNet.Identity;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Net;
+using System.Net.Http;
 using System.Web.Http;
-using System.Web.Mvc;
-using static System.Net.Mime.MediaTypeNames;
-using HttpPostAttribute = System.Web.Mvc.HttpPostAttribute;
+using bigSchool.DTOs;
+using bigSchool.Models;
+using Microsoft.AspNet.Identity;
 
 namespace bigSchool.Controllers
 {
-    public class FollowingsController : Controller
+    public class FollowingsController : ApiController
     {
         private readonly ApplicationDbContext _dbContext;
         
@@ -27,7 +25,7 @@ namespace bigSchool.Controllers
             var userId = User.Identity.GetUserId();
             if (_dbContext.Followings.Any(f => f.FollowerId == userId && f.FolloweeId == followingDto.FolloweeId))
             {
-                return BadRequest
+                return BadRequest("Following already exists!");
             }
                 
             var following = new Following
